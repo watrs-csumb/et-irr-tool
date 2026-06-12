42598a1ef4ae784f0f6ee4dda1582d5ad30b4c07
Jose Rodriguez <59034788+josemrodriguezf@users.noreply.github.com>
2026-06-12 10:05:19 -0700

    Update app.R

--- app.R
+++ app.R
@@ -1858,7 +1858,8 @@ server <- function(input, output, session) {
         var d = this.data();
         var stale = parseInt(d[ncols - 1]);
         if (!isNaN(stale) && stale >= 2) {
-          $(this.node()).css({'background-color': '#fde8e8', 'color': '#7b1212'});
+          $(this.node()).css({'background-color': '#fde8e8'});
+          $(this.node()).find('td').css({'color': '#c0392b', 'font-weight': '600'});
         }
       });
     ")
